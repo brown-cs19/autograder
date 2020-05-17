9ccdd2604f615b5444848f38177d44364476160b
mxheller <heller.max1@gmail.com>
2020-05-17 19:04:41 -0400

    update setup.sh

--- gradescope/setup.sh
+++ gradescope/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt update
+apt update
 
 curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install -y python3 jq build-essential nodejs
+bash nodesource_setup.sh
+apt install -y make python3 jq build-essential nodejs npm
 
 git clone https://github.com/mxheller/cs19-autograding.git --recurse-submodules
 cd cs19-autograding/powder-monkey/pyret-lang
