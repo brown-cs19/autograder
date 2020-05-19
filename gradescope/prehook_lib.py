bc78a7e5608e8b56614082c836fdeb7e93d24a65
mxheller <heller.max1@gmail.com>
2020-05-19 08:46:58 -0400

    fix encoding

--- gradescope/prehook_lib.py
+++ gradescope/prehook_lib.py
@@ -9,7 +9,7 @@ class ImportFixer:
         self.stencil_dir = stencil_dir
         self.rel_stencil_dir = os.path.relpath(self.stencil_dir,
                                                start=self.target_dir)
-        with open(target_path, 'r') as f:
+        with open(target_path, 'r', encoding="utf-8") as f:
             self.content = f.read()
 
     def fix_import(self, name, location, filename=None):
@@ -23,5 +23,5 @@ class ImportFixer:
         self.content = re.sub(r'shared-gdrive\(["\'](.*?)["\'].*?\n?.*?\)',
                               rf'file("{self.rel_stencil_dir}/\1")',
                               self.content, re.M)
-        with open(self.target_path, 'w') as f:
+        with open(self.target_path, 'w', encoding="utf-8") as f:
             f.write(self.content)
