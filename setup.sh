18f78102465721a0d8a935e57dfc81c70cde19e9
mxheller <max.a.heller@gmail.com>
2020-05-20 16:54:14 -0400

    Switch over to Thomas's Typescript implementation

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 
 apt update
 
+# Install node and npm
 curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
+# Clone result processing repo
+cd /autograder/results
+npm install -D typescript ts-node @types/node
+git clone https://github.com/tdelv/result-processing.git
+
+# Unpack pyret zip
 unzip "/autograder/source/autograder/pyret-lang.zip" -d /autograder/pyret-lang
