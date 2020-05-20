cef40a37d88017a85dab097aa1f5902524f0c97e
mxheller <max.a.heller@gmail.com>
2020-05-20 17:29:26 -0400

    Install typescript globally

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ apt install -y make python3 jq build-essential nodejs npm unzip
 # Clone result processing repo
 mkdir -p /autograder/results
 cd /autograder/results
-npm install -D typescript ts-node @types/node
+npm install -g typescript ts-node @types/node
 git clone https://github.com/tdelv/result-processing.git
 
 # Unpack pyret zip
