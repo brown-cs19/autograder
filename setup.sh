c149b368d345b646c236770b7c7495099b55f549
mxheller <max.a.heller@gmail.com>
2020-05-23 12:13:53 -0400

    Relocate result-processing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt update
+cd /autograder
 
 # Install node and npm
 curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
@@ -8,10 +9,8 @@ bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
-mkdir -p /autograder/results
-cd /autograder/results
 npm install -g typescript ts-node @types/node
 git clone https://github.com/tdelv/result-processing.git
 
 # Unpack pyret zip
-unzip "/autograder/source/autograder/pyret-lang.zip" -d /autograder/pyret-lang
+unzip "source/autograder/pyret-lang.zip" -d pyret-lang
