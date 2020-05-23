350e8a622857fe6ce78493ac0e4116427b29d796
mxheller <max.a.heller@gmail.com>
2020-05-23 13:38:09 -0400

    install @types/node in the right directory

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
-npm install -g typescript ts-node @types/node
+npm install -g typescript ts-node
 git clone https://github.com/tdelv/result-processing.git
 
 # Unpack pyret zip
