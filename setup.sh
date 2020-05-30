2b9447c9180612b5125d8733d708dbe3fbea8cbf
Max Heller <max_heller@brown.edu>
2020-05-30 14:07:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
 npm install -g typescript ts-node
-git clone https://github.com/tdelv/result-processing.git
+git clone https://github.com/brown-cs19/result-processing.git
 
 # Unpack pyret zip
 unzip "source/autograder/pyret-lang.zip" -d pyret-lang
