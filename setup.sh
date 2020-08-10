e02d7053c2a674d40c1e5526e1479773170332b3
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2020-08-10 15:22:14 -0400

    Temporary logging addition.

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
+echo "Cloning result-processing"
 npm install -g typescript ts-node
 git clone https://github.com/brown-cs19/result-processing.git
 
