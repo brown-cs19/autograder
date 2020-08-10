ca9ec0abdc093252ac0d795340540c7124af291d
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2020-08-10 15:58:57 -0400

    Fixed command to point to correct point file.

--- setup.sh
+++ setup.sh
@@ -9,10 +9,8 @@ bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
-echo "Cloning result-processing"
 npm install -g typescript ts-node
 git clone https://github.com/brown-cs19/result-processing.git
 
 # Unpack pyret zip
-echo "Unpacking pyret"
 unzip "source/autograder/pyret-lang.zip" -d pyret-lang
