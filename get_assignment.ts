239b2a0f91985a7386cd7dcfa55763aa22b99fad
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-09-06 10:12:18 -0400

    Add assignment getter.

---
+++ get_assignment.ts
@@ -0,0 +1,24 @@
+function parse_command_line(): string {
+    let args: string[] = process.argv.slice(2);
+
+    if (args.length != 1) {
+        throw("Usage: <meta_data_file>");
+    }
+
+    return args[0];
+}
+
+function load_file(path: string) {
+    let fs = require('fs');
+    let contents: string = fs.readFileSync(path);
+    return JSON.parse(contents);
+}
+
+function main() {
+    let meta_data_file: string = parse_command_line();
+    let data = load_file(meta_data_file);
+
+    console.log(data.assignment.title)
+}
+
+main();
