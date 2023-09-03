b4aa5e5c8a400810ea0322bd97d68ac12be37a58
Joe Dodson <74784507+jcvdodson@users.noreply.github.com>
2023-09-03 12:46:52 -0400

    try updating nodeJS version

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ apt update
 cd /autograder
 
 # Install node and npm
-curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
-bash nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
 apt install -y make python3 jq build-essential nodejs npm unzip
 
 # Clone result processing repo
