b57e473ec0b5f23a2af556ef121b5e69a115da8a
Dorai Sitaram <ds26gte@gmail.com>
2025-09-07 15:56:58 -0400

    setup.sh: install more recent (20) version of node

--- setup.sh
+++ setup.sh
@@ -8,6 +8,12 @@ curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 20
+nvm use 20
+ln -sf "$(command -v node)" /usr/bin/nodejs #symlink to node
+
 # Clone result processing repo
 npm install -g typescript@5.0 ts-node
 git clone https://github.com/brown-cs19/result-processing.git
