649f1d09f26c4f11297517e28225507993e8c2cd
Dorai Sitaram <ds26gte@gmail.com>
2025-09-07 16:45:39 -0400

    setup.sh: install node 24

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ apt install -y make python3 jq build-essential nodejs npm unzip
 
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
-nvm install 20
-nvm use 20
+nvm install 24
+nvm use 24
 ln -sf "$(command -v node)" /usr/bin/nodejs #symlink to node
 
 # Clone result processing repo
